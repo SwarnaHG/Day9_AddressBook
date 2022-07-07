d5500e91645160023bfc7e2d2b18b9cd978cf47b
SwarnaHG <swarnahg18@gmail.com>
2022-07-07 23:01:54 +0530

    uc2 Console to take user inputs

--- src/AddressBookday9/AddressBook.java
+++ src/AddressBookday9/AddressBook.java
@@ -1,16 +1,10 @@
 package AddressBookday9;
+import java.util.Scanner;
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Address Book System ");
-        contactDetails newContact = new contactDetails();
-        newContact.setFirstName("Swarna \n");
-        newContact.setLastName("Amith \n");
-        newContact.setAddressCity("Btm Layout \n");
-        newContact.setState("Karnataka\n");
-        newContact.setZip(570026);
-        newContact.setPhoneNumber("9343305151 \n");
-        newContact.setEmail("swarnahg18@gmail.com \n");
-        System.out.println("The Contact is : \n" + newContact);
+       // System.out.println("Address Book System Program");
+        newAddressBook person = new newAddressBook();
+        person.addContact();
     }
 }
 class contactDetails {
@@ -22,50 +16,98 @@ class contactDetails {
     private String phoneNumber;
     private String email;
 
+    //get set method for contact details
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getAddressCity() {
         return addressCity;
     }
+
     public void setAddressCity(String addressCity) {
         this.addressCity = addressCity;
     }
+
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public int getZip() {
         return zip;
     }
+
     public void setZip(int zip) {
         this.zip = zip;
     }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String toString() {
         return " FirstName :" + firstName + " LastName :" + lastName + " Address City : " + addressCity + " State : "
                 + state + " Zip :" + zip + '\'' + " PhoneNumber : " + phoneNumber + " Email : " + email;
+
     }
 }
+
+    //Console to get the user input by scanner class
+    class newAddressBook {
+        Scanner sc = new Scanner(System.in);
+
+        public void addContact() {
+            contactDetails person = new contactDetails();
+            System.out.println("Enter First Name: ");
+            String firstName = sc.nextLine();
+            System.out.println("Enter last Name: ");
+            String lastName = sc.nextLine();
+            System.out.println("Enter your addressCity: ");
+            String addressCity = sc.nextLine();
+            System.out.println("Enter your state: ");
+            String state = sc.nextLine();
+            System.out.println("Enter zip code : ");
+            Long zip = sc.nextLong();
+            System.out.println("Enter phone number: ");
+            String phoneNumber = sc.nextLine();
+            System.out.println("Enter your EMail ID: ");
+            String email = sc.nextLine();
+            person.setFirstName(firstName);
+            person.setLastName(lastName);
+            person.setAddressCity(addressCity);
+            person.setState(state);
+            person.setZip(zip.intValue());
+            person.setPhoneNumber(phoneNumber);
+            person.setEmail(email);
+            System.out.println("The Contact Details of " + firstName + "\n" + person);
+        }
+    }
+
